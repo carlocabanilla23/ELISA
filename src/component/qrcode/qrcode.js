086c49f49e0228c6d8eafc5e45db31c9570caba5
carlocabanilla23 <carlocabanilla23@gmail.com>
2023-01-08 23:41:41 -0800

    fixed issue with the qrcode

--- src/component/qrcode/qrcode.js
+++ src/component/qrcode/qrcode.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import {QRCodeSVG} from 'qrcode.react';
 import { createRoot } from 'react-dom/client';
 import logo from '../icons/spu-logo.png';
+import qrCode from 'qr-code-styling';
 
 
 export const Generate  = (e) => {
@@ -9,19 +10,20 @@ export const Generate  = (e) => {
     const path = "https://dev.djno0p84ctg6u.amplifyapp.com/ViewItemInfo/"+ e;
     console.log(path);
     return(
+
+       
         <QRCodeSVG
         value={path}
         size={350}
         bgColor={"#ffffff"}
         fgColor={"#000000"}
-        level={"L"}
         includeMargin={false}
         imageSettings={{
             src: "https://spu.edu/style-guide/assets/branding/logo-spu-flame-legacy-maroon.png",
             x: undefined,
             y: undefined,
-            height: 100,
-            width: 100,
+            height: 75,
+            width: 75,
             excavate: true,
         }}
         />
