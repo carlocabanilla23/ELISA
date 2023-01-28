8cbb2f7be4fa87a8be1e82f1e9a8a6a5430a101b
John Cabanilla <cabanillaj@spu.edu>
2023-01-28 11:26:59 -0800

    added mailer

---
+++ src/component/mailer/mailman.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const mailman = (data) => {
+
+    let mailTransporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 587, false for other ports
+        requireTLS: true,
+        auth : {
+            user: "SPU.Elisa@gmail.com",
+            pass: "svitawseyjrkqgav"
+        }
+    });
+
+    let content = {
+        from: "SPU.Elisa@gmail.com",
+        to: "samd@spu.edu",
+        subject : "test nodemailer",
+        text : "it is working"
+    }
+
+    mailTransporter.sendMail(content,(err)=>{
+        if (err)
+            console.log("error occured" + err);
+        else
+            console.log("success");
+    });
+}
+
+export default mailman;
