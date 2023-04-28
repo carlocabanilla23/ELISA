d334aa1e28d88ce270694f5d0c24b441814d2561
DatSam04 <samd@spu.edu>
2023-04-28 12:55:12 -0700

    implement import items from xlsx file

--- src/pages/Inventory/AddItem.js
+++ src/pages/Inventory/AddItem.js
@@ -8,12 +8,16 @@ import { DefaultDeviceLogo } from "../../assets/base64/base64imgs";
 import useItems from "../../hooks/useItems";
 import { GetDateToday } from "../../Services/etc/GetDateToday";
 import Validate from "../../Services/form-validation/error-codes";
+import { read, utils} from 'xlsx';
 Amplify.configure(awsExport);
 
 function AddItem() {
     const [RFIDCode2, setRFIDCode2] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const { items } = useItems();
+
+    const hiddenImportFile = React.useRef(null);
+
     const navigate = useNavigate();
 
     const [data,setData] = useState({
@@ -47,7 +51,7 @@ function AddItem() {
 
     // AddItem function is called when the form is submitted
     const AddItem = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
         let param = { RFIDCode2 }
         let err = Validate(items,data,param);
         let rm;
@@ -57,6 +61,7 @@ function AddItem() {
         if (err.length > 0) {
             setErrorMessage(err);
         } else {
+            // console.log(data);
             API.post("items","/items/", {
             // call the API to post the item's information to the inventory
                 body : data
@@ -98,6 +103,71 @@ function AddItem() {
         });
     }
 
+    const importCSV = () => {
+        console.log("Import File");
+        hiddenImportFile.current.click();
+    }
+
+    const importData = async (e) => {
+        e.preventDefault();
+        var automatedRFID = 0;
+        await API.get("items","/items").then( itemRes => {
+            automatedRFID = itemRes.length + 1;
+        });
+        const inputFile = e.target.files;
+        if(inputFile){
+            const file = inputFile[0];
+            const reader = new FileReader();
+            reader.onload = (input) => {
+                const workbook = read(input.target.result);
+                const sheets = workbook.SheetNames;
+                if(sheets){
+                    const rows = utils.sheet_to_json(workbook.Sheets[sheets[0]]);
+                    rows.map(async (row) => {
+                        if(row.Column1){
+                            const startIndex = row['Handheld Devices'].indexOf(row.Column3) + row.Column3.toString().length + 1;
+                            const end = row['Handheld Devices'].length;
+                            const inputType = row['Handheld Devices'].substring(startIndex, end);
+                            await API.post("items","/items/", {
+                                body: {
+                                    name : row.Column1,
+                                    serialno : row.Column5,
+                                    type : inputType,
+                                    model : row.Column3.toString(),
+                                    location : "Room",
+                                    roomno : row.Column4.toString(),
+                                    status : "Available",
+                                    cost : 200,
+                                    acquiredate : GetDateToday(),
+                                    createdate: GetDateToday(),
+                                    lastupdated: GetDateToday(),
+                                    rfidcode : automatedRFID.toString(),
+                                    image : DefaultDeviceLogo(),
+                                    manufacturer : row.Column2,
+                                    condition : "New",
+
+                                    expiredate : "NA",
+
+                                    assigndate : "NA",
+                                    assignedto : "NA",
+
+                                    returndate : "NA",
+                                    reviewedby : "NA",
+
+                                    prevroomno : "NA",
+                                    prevlocation : "NA"
+                                }
+                            });
+                            automatedRFID++;
+                        }
+                    })
+                }
+            }
+            reader.readAsArrayBuffer(file);
+        }
+        ShowAlert();
+    }
+
     return (
         <>
             <div className="alert alert-success" id="alert" role="alert">
@@ -112,6 +182,18 @@ function AddItem() {
                         <label>Add Item</label>
                         </div>
                     </div>
+            </div>
+            {/* Import Button */}
+            <div className="col-auto-dropdown">
+                <div className="dropdown">
+                    <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Import
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li><button className="dropdown-item" onClick={importCSV}>CSV</button></li>
+                        <input ref={hiddenImportFile} type="file" accept=".xlsx" onChange={importData} style={{display: 'none'}} />
+                    </ul>
+                </div>
             </div>
             {/* Add Item Form */}
             <div className="add-item-form-wrapper">
@@ -240,7 +322,6 @@ function AddItem() {
                                 <div className="col-sm-10">
                                     <input type="text" className="form-control" id="cost" name="cost"
                                     value={data.cost} onChange={ e => ChangeData(e)} />
-                   
                                 </div>
                             </div>
                             {/* Date Acquired */}
@@ -255,7 +336,7 @@ function AddItem() {
                             <div className="form-group row my-2">
                                 <label className="col-sm-2 col-form-label" for="RFIDCode" >RFID Code</label>
                                 <div className="col-sm-10">
-                                    <input type="text" className="form-control" name="rfidcode"
+                                    <input type="text" className="form-control" name="rfidcode" id="rfidcode"
                                     value={data.rfidcode} onChange={ e => ChangeData(e)}/>
                                 </div>
                             </div>
@@ -286,7 +367,7 @@ function AddItem() {
                         <div id="lastCheck" className="add-item-action-btn">
                             <span id="lastCheck" className="errormessage">{errorMessage}</span>
                             <button className="button" type ="button" onClick={CancelEdit} >Cancel</button>
-                            <button className="button" type ="submit" >Save item</button>
+                            <button className="button" type ="submit" id="CreateItem" >Save item</button>
                         </div>
                     </div>
                 </form>
